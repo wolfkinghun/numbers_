946ba8642299b334ba4bf61e3f7fbc1d1234b5a3
Henlorata <matuzabalazs@gmail.com>
2024-03-22 09:58:53 +0100

    compare function, rename JS file

---
+++ js.js
@@ -0,0 +1,9 @@
+export function genrandNumber(min, max) {
+    return Math.floor(Math.random()*(max-min+1))+min
+}
+
+export function compare(guessedNr, randomNr) {
+    if (guessedNr > randomNr) return "A te számod nagyobb."
+    else if (randomNr > guessedNr) return "A te számod ksiebb."
+    else return "Helyes megfejtés."
+}
